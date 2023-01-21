8130bdc396b0eadf345f942249754b3881345880
IshaanSurve <107930986+IshaanSurve@users.noreply.github.com>
2023-01-21 18:03:12 +0530

    test

---
+++ app/src/main/java/com/example/oakridge/family_code_guardian.java
@@ -0,0 +1,14 @@
+package com.example.oakridge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class family_code_guardian extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_family_code_guardian);
+    }
+}
